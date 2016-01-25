0e854327e2dd504533c171a0667b84237703bd3e
hessenh <hansolav_91@hotmail.com>
2016-01-25 13:13:52 +0100

    Path

--- python/weka.py
+++ python/weka.py
@@ -21,10 +21,11 @@ def weka_main(direct, generalized,window_size, without_activity):
 	df_features = pd.read_csv(FEATURES)
 
 	if generalized:
-		LABELS = os.path.join(dirname(dirname(__file__)), 'data/'+direct + '/DATA_WINDOW/'+str(window_size)+'/ORIGINAL/Usability_LAB_All_L_GENERALIZED.csv')
+		LABELS = os.path.join(dirname(dirname(__file__)), 'data/'+direct + '/DATA_WINDOW/'+str(window_size)+'/ORIGINAL/GoPro_LAB_All_L_GENERALIZED.csv')
 	else:
-		LABELS = os.path.join(dirname(dirname(__file__)), 'data/'+direct + '/DATA_WINDOW/'+str(window_size)+'/ORIGINAL/Usability_LAB_All_L.csv')
+		LABELS = os.path.join(dirname(dirname(__file__)), 'data/'+direct + '/DATA_WINDOW/'+str(window_size)+'/ORIGINAL/GoPro_LAB_All_L.csv')
 
+	print LABELS
 	df_labels = pd.read_csv(LABELS)
 	df_labels.columns = ['label']
 
@@ -33,8 +34,12 @@ def weka_main(direct, generalized,window_size, without_activity):
 
 	df_features = pd.concat([df_features,df_labels],axis=1)
 
+	activities = []
+	#activities = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
 	if without_activity:
-		df_features = df_features[df_labels['label'] != 9]
+		print "he"
+		for activity in activities:
+			df_features = df_features[df_labels['label'] != activity]
 
 	if generalized:
 		t = os.path.join(dirname(dirname(__file__)), 'data/'+direct + '/WEKA/' + str(window_size) +'/'+ direct + 'GENERALIZED.arff')
@@ -54,7 +59,15 @@ def weka_main(direct, generalized,window_size, without_activity):
 	if generalized:
 		label_pos = ["@attribute act {1.0,2.0}"]
 	else: 
-		label_pos = ["@attribute act {1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0,16.0,17.0,18.0}"]#["@attribute act {1.0,6.0,7.0,8.0,9.0}"] #
+		#label_pos = ["@attribute act {1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0,16.0,17.0,18.0}"]#["@attribute act {1.0,6.0,7.0,8.0,9.0}"] #
+		label_pos = ["@attribute act {1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0,16.0,17.0,18.0,19.0, 20.0, 21.0, 22.0, 23.0, 24.0, 25.0, 26.0, 27.0, 28.0, 29.0, 30.0, 31.0, 32.0, 33.0, 34.0, 35.0, 36.0, 37.0, 38.0, 39.0, 40.0, 41.0, 42.0, 43.0, 44.0, 45.0, 46.0, 47.0, 48.0, 49.0, 50.0, 51.0, 52.0, 53.0, 54.0, 55.0, 56.0, 57.0, 58.0, 59.0, 60.0, 61.0, 62.0, 63.0, 64.0}"]
+		label_string = "@attribute act {"
+		for i in range(1,65):
+			if i not in activities:
+				label_string = label_string + str(i) + ".0,"
+		label_pos = [label_string[:len(label_string)-1] + "}"]
+
+
 	# ["@attribute act {1.0,3.0,6.0,7.0,8.0,9.0,10.0,12.0}"]
 	finalList.append(label_pos)
 	finalList.append([''])
